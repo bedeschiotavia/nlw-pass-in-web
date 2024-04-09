4d7e907000fab64e83f9436d0e8e9a98ae9b7789
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-04-09 15:37:51 +0100

    Setup tailwindcss

---
+++ src/components/attendee-list.tsx
@@ -0,0 +1,7 @@
+export function AttendeeList (){
+    return (
+      <div>
+        <h1>Attendee</h1>
+      </div>
+    )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import nlwUniteIcon from '../assets/nlw-unite-icon.svg'
+
+export function Header () {
+
+  return (
+    <div>
+      <img src={nlwUniteIcon} />
+      <nav>
+        <a href="">Events</a>
+        <a href="">Attendees</a>
+      </nav>
+    </div>
+  )
+  
+}
